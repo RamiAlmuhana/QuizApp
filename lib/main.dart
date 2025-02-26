6faa56f05685c7b57a3d0152f5a627d510b359e8
RamiAlmuhana <ramealmuhana@hotmail.com>
2025-02-26 19:29:10 +0100

    InitState gebruikt

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:quiz_app/start_screen.dart';
+import 'package:quiz_app/quiz.dart';
+
 
 void main() {
-  runApp(
-     MaterialApp(
-      home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color.fromARGB(255, 48, 8, 117), 
-                Color.fromARGB(255, 87, 20, 202)
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-          ),
-          child: const StartScreen(),
-        ),
-      ),
-    ),
-  );
+  runApp(const Quiz());
 }
